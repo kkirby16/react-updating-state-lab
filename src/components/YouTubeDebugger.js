d4e035e580112db26d6675661b22c3c1040d5d76
kkirby16 <kkirby16@my.whitworth.edu>
2021-07-20 17:16:54 -0700

    commit tree 19838a6a9aded56f6dc9a9d27aac462227318390

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,67 @@
 // Code YouTubeDebugger Component Here
+
+import React, { Component } from "react";
+
+export default class YoutubeDebugger extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: "1080p",
+        },
+      },
+    };
+  }
+
+  changeBitrate = () => {
+    this.setState({
+      settings: Object.assign({}, this.state.settings, {
+        bitrate: 12,
+      }),
+    });
+  };
+
+  changeResolution = () => {
+    this.setState(
+      {
+        settings: Object.assign({}, this.state.settings, {
+          bitrate: 8,
+          video: {
+            resolution: "720p",
+          },
+        }),
+      },
+      () => console.log(this.state)
+    );
+  };
+
+  //   changeResolution = () => {
+  //     this.setState(
+  //       {
+  //         video: Object.assign({}, this.state.settings.video, {
+  //           resolution: "720p",
+  //         }),
+  //       },
+  //       () => console.log(this.state)
+  //     );
+  //   };
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.changeBitrate}>
+          Change Bitrate
+        </button>
+
+        <button className="resolution" onClick={this.changeResolution}>
+          Change Resolution
+        </button>
+      </div>
+    );
+  }
+}
